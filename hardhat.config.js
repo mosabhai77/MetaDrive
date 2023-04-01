c2354007bca74e591740f27fa629f72907224be4
mosabhai77 <mahigunda230@gmail.com>
2023-04-01 22:29:00 +0530

    second commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-  networks: {
-    hardhat: {
-      chainId: 1337,
+    solidity: "0.8.9",
+    networks: {
+        hardhat: {
+            chainId: 1337,
+        },
     },
-  },
-  paths: {
-    artifacts: "./client/src/artifacts/",
-  },
+    paths: {
+        artifacts: "./client/src/artifacts/",
+    },
 };
